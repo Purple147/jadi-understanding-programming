fc74932f00024e944ec406595fa31819feb8e8cd
Purple147 <gorcraft1380@gmail.com>
2025-11-26 08:39:03 +0330

    chore: 0 begginer: changing name to 0.py

---
+++ 0 begginer/0.py
@@ -0,0 +1,30 @@
+ali = "sina"
+ali.zfill(7)
+ali
+hamid = 7
+float(hamid)
+hamid
+b =3209472384
+kazem = "zade"
+a = [hamid, ali, "karim", 99]
+s = {hamid, ali, "karim", 99}
+d = {hamid: 97398247, ali: b, "karim": 32285888, 99: str(324732894)} 
+a
+s
+d
+mohammad = 4
+hamid + mohammad + 1
+ali + kazem + str(1)
+hamid - mohammad - 1
+hamid * mohammad * 1
+hamid / mohammad / 1
+hamid % mohammad % 1
+hamid ** mohammad ** 1
+hamid **0.5, mohammad **0.5, 1 **0.5
+hamid != mohammad != 1
+ali != "sina"
+ali
+ali != "karim"
+hamid < mohammad > 1
+hamid > mohammad > 1
+hamid > mohammad != 1 < b != ali
