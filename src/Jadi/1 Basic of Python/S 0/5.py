c2fbbbaa6db8f8f72a859528aa18d645bd600040
Purple147 <gorcraft1380@gmail.com>
2025-12-14 09:35:52 +0330

    refactor: elif: and input()

--- src/Jadi/1 Basic of Python/S 0/5.py
+++ src/Jadi/1 Basic of Python/S 0/5.py
@@ -5,12 +5,6 @@ type("sdfashdf")
 Mohammad = "Programmer"
 mohammad = 24
 Roxana = 18
-if Mohammad == "Programmer":
-    status = True
-else:
-    status = False
-
-
-print(status, type(status), 5 > 4, 11 <= 7, 22 == mohammad, mohammad != 22, not mohammad == 23)
+print(type(True), 5 > 4, 11 <= 7, 22 == mohammad, mohammad != 22, not mohammad == 23)
 print(mohammad > 20 and Roxana > 20, Roxana > 20 and mohammad > 20)
 print(mohammad > 20 or Roxana > 20)
