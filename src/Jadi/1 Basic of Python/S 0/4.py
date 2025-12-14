36397a79c9d1a8d6d5f55afa9c74d72f9f2d7b24
Purple147 <gorcraft1380@gmail.com>
2025-12-14 08:37:35 +0330

    refactor: and or not

--- src/Jadi/1 Basic of Python/S 0/4.py
+++ src/Jadi/1 Basic of Python/S 0/4.py
@@ -2,7 +2,7 @@
 Mohammad = "Programmer"
 mohammad = 24
 print("mohammad is a", Mohammad, "by", mohammad, "Years old")
-Future_Mohammad = (mohammad / 4 )** 2
+Future_Mohammad = (mohammad / 4) ** 2
 print(Future_Mohammad)
 Future_Mohammad += 2
 print(Future_Mohammad)
@@ -18,7 +18,7 @@ print(Two_Mohammad_1, Two_Mohammad_2)
 Mohammad_2 = "Purple147"
 print(Mohammad_2 + Mohammad)
 print(Mohammad_2 + " " + Mohammad)
-Commend ="Please give me your age: "
+Commend = "Please give me your age: "
 age = input(Commend)
 type(age)
 age = int(age)
