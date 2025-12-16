f00143e130e99fc38d6ffc6499c42f9797b0bbd3
Purple147 <gorcraft1380@gmail.com>
2025-12-17 02:14:32 +0330

    learning: Libraries(Packages)

---
+++ src/Jadi/1 Basic of Python/S3/1.py
@@ -0,0 +1,6 @@
+# Rading Standard Library of language we use, making account in stackoverflow, datetime.date.today(), type(), pip install fo libraries, sudo
+# ipython, our secret wipen in python is Libraries
+from datetime import date
+Today = date.today()
+print(Today, type(Today))
+
