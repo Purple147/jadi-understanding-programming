da6956cf5b292466b535bc880fef70e7a2ef6419
Purple147 <gorcraft1380@gmail.com>
2025-12-17 01:14:56 +0330

    Learning: Standard Libraris

---
+++ src/Jadi/1 Basic of Python/S3/0.py
@@ -0,0 +1,25 @@
+# Libraris, import, random, math, Libraris is codes going to packaging for peoples use, Standard libraries(Python give you by installing python)
+# 0 Core 1 Buid in Functi 2 Libraries Fuctions and Libraries, floor(), statistics, mean([x, y]), zipfile, reading standard python library
+# form x import y
+import random
+
+random.seed()
+random.random()
+random.randint(0, 10)
+random.randrange(10)
+
+import math
+
+math.sin(0.5)
+print(math.floor(23.8432523222991))
+
+import statistics
+
+print(statistics.mean([0, -22, 2, 6, 77, 1, 23, 20]))
+
+import zipfile
+
+from random import randint
+from random import randrange
+print(randint(30, 44))
+print(randrange(333))
