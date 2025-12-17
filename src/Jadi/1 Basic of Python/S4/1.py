16bc3f456c3d6297aa3ab4a642ab274fc9cc5c01
Purple147 <gorcraft1380@gmail.com>
2025-12-18 03:29:43 +0330

    Learning: csv

---
+++ src/Jadi/1 Basic of Python/S4/1.py
@@ -0,0 +1,18 @@
+# CSV(Comma Sepreited Values), CSV files using for sending datas, read examples in documents, doing different to seeing
+# for power in partition, statistic, mean([x,y]), list(), founding meaning of they numbers
+# don't think complete way for codin just doing and way is showed hemlesf(Flowing Natural), %1.2f, %4s
+import csv
+from statistics import mean
+
+with open(
+    "E:/0 Plans/1 Job(Money)/3 Platforms/0 Git, GitHub/1 Repos/Jadi/src/Jadi/1 Basic of Python/S4/1.csv"
+) as f:
+    reader = csv.reader(f)
+    for vibration in reader:
+        Names = vibration[0]
+        Listing = list()
+        for vibration_2 in vibration[1:]:
+            vibration_2 = int(vibration_2)
+            Listing.append(vibration_2)
+            Mean = mean(Listing)
+        print("Average Number of %11s is %1.2f" % (Names, Mean))
