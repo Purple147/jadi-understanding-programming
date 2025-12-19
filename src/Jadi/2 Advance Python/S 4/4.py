eee735870cb14bafab00bb7a4eeede5c16251f79
Purple147 <gorcraft1380@gmail.com>
2025-12-19 21:07:11 +0330

    Learning: Security

---
+++ src/Jadi/2 Advance Python/S 4/4.py
@@ -0,0 +1,9 @@
+# Security, Security job is not funny like movies, Security guys not popular(Peoples not like this guys)
+# People like movies security(Black Hats), Good package for security is scapy,
+# in reality White Hats is better than Blacks, Black Hats allyas going to prison or bloffing
+# Good Book(+Have Course too) = Python for Offensive PenTest(Udemy), 
+# Good Book(+Have Course too) = Python for Penetrations and Hackers(SecurityTube)
+# Security for learn and do is not funny and amazing
+# Security World not learn you to Hack this thing, Just give us tools to do somethings
+# in Security World no one learn you big things, Just give us idias and tools
+# in Security World we most to discoverd(Found) big things
