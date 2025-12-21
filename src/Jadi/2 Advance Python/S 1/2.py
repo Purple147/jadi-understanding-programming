d798a9b222125088f97bca69f3faccd8cf02f7cd
Purple147 <gorcraft1380@gmail.com>
2025-12-21 16:26:14 +0330

    learning: object oriented programming

---
+++ src/Jadi/2 Advance Python/S 1/2.py
@@ -0,0 +1,10 @@
+# Object Oriented Programming(OOP), C++ is OOP for C,OOP in Python: x = q, w, e, ... and y = x = s, d, f,... and z = g,... and j = g,v,...
+# f = j = y = x = t, e, w, q,..., So we can say OOP kind of connected to Alising
+Variables = ["Class Variable", "Object Variable"]
+Object = [Variables, "Methodes"]
+Example = Class = Person = "Numbers"
+Contunue_Example = Object = Person = "Name", "Age", ...
+Example2 = Jadi = Person()
+# Jadi is a Object of Person Class, in int(Number) we can say int is a class for number object
+Object = Class()
+Milano = "Sigar()"
